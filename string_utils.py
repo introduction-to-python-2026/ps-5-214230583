feaf897e8bea13890295f151cdc9962efeb8da60
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-11-25 15:36:16 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -2,10 +2,38 @@
 
 
 def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+    split_formula = []
+    if not formula:
+        return split_formula
 
+    start_index = 0
+    for i in range(1, len(formula)):
+        if formula[i].isupper():
+            split_formula.append(formula[start_index:i])
+            start_index = i
+
+    split_formula.append(formula[start_index:])
+    return split_formula
 def split_at_digit(formula):
-    pass  # replace the pass with your code
+    digit_location = 1 
+
+    
+    for i in range(1, len(formula)):
+        if formula[i].isdigit():
+          
+            digit_location = i
+            break 
+        else:
+          
+            digit_location += 1
+
+  
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        prefix = formula[:digit_location]
+        numeric_part = int(formula[digit_location:])
+        return prefix, numeric_part
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
